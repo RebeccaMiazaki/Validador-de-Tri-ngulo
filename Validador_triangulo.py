11bb468709cee6b7cf8fa13cabad27aa247a24eb
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-10-16 16:54:59 -0300

    - commit thais

--- Validador_triangulo.py
+++ Validador_triangulo.py
@@ -1,6 +1,6 @@
 # Validador triangulo
 
-#Apresentação..
+#Apresentação
 print('\n\t\t\t -- Validador de Triângulo -- \n\n')
 
 #Entrada
@@ -9,8 +9,18 @@ b = int(input('Informe o lado B: '))
 c = int(input('Informe o lado C: '))
 
 #Processamento e Saída
-if a < (b + c) and c < (a + b):
+if a < (b + c) and b < (a+b) and c < (a + c):
     print(f'\n\n {a}, {b} e {c} formam um Triângulo!')
 
+if a == (b+c) or b == (a+c) or == c(a+b)
+    print(f'\n\n {a} , {b} e {c} é um triangulo equilaterio')
+
+if a == b  or b == a or != c(a+b)
+    print('\n\n {a} , {b} e {c} é um triangulo isósceles')
+
+if  a != (b+c)  or b != (a+c) or != c(b+a)
+    print('f \n\n {a}, {b} e {c} é um triangulo escaleno')
+
 else:
-    print('\n\n Não é um Triângulo.')
+    print('\n\n não é um traingulo ')
+
