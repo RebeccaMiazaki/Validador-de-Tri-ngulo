b01b104b9e9e17bf4676dd471bb3016d869e39c4
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-10-16 16:01:13 -0300

    - Commit Rebecca

---
+++ Validador_triangulo.py
@@ -0,0 +1,16 @@
+# Validador triangulo
+
+#Apresentação
+print('\n\t\t\t -- Validador de Triângulo -- \n\n')
+
+#Entrada
+a = int(input('Informe o lado A: '))
+b = int(input('Informe o lado B: '))
+c = int(input('Informe o lado C: '))
+
+#Processamento e Saída
+if a < (b + c) and c < (a + b):
+    print(f'\n\n {a}, {b} e {c} formam um Triângulo!')
+
+else:
+    print('\n\n Não é um Triângulo.')
